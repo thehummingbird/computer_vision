8c9d71739131e47d62c8ae0fd10e5d1a9d00993b
thehummingbird <sharadmaheshwari19@gmail.com>
2024-09-27 23:04:21 +0200

    feat tune yolo tracker

--- yolov10_detection_and_tracking/yolo_detector.py
+++ yolov10_detection_and_tracking/yolo_detector.py
@@ -2,14 +2,16 @@ from ultralytics import YOLO
 import cv2
 import time
 from deep_sort_realtime.deepsort_tracker import DeepSort
+
+CONFIDENCE = 0.2
 # Load a pre-trained YOLOv10n model
-model = YOLO("yolov10n.pt")
+model = YOLO("yolov10m.pt")
 
 object_tracker = DeepSort(
-    max_age=50,
+    max_age=20,
     n_init=2,
-    nms_max_overlap=1.0,
-    max_cosine_distance=0.3,
+    nms_max_overlap=0.3,
+    max_cosine_distance=0.8,
     nn_budget=None,
     override_track_class=None,
     embedder="mobilenet",
@@ -21,6 +23,8 @@ object_tracker = DeepSort(
     today=None
 )
 
+classList = ["person"]
+
 
 def make_detections(boxes):
     detections = []
@@ -29,7 +33,9 @@ def make_detections(boxes):
         x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
         w, h = x2 - x1, y2 - y1
         clsNum = int(box.cls[0])
-        print(clsNum)
+
+        if result.names[clsNum] not in classList:
+            continue
         conf = box.conf[0]
         # print(box)
         detections.append((([x1, y1, w, h]), clsNum, conf))
@@ -43,31 +49,46 @@ def make_detections(boxes):
 
 
 # Open the video file
-cap = cv2.VideoCapture("video.mp4")
+cap = cv2.VideoCapture("football.mp4")
 
 if not cap.isOpened():
     print("Error: Unable to open video file.")
     exit()
+
+# for _ in range(500):
+#     ret, frame = cap.read()
+#     if not ret:
+#         break
 
 # Read frames one by one
 while True:
     ret, frame = cap.read()
     if not ret:
         break
+
+    # height, width, _ = frame.shape
+
+    # # Calculate the new dimensions (half the original size)
+    # new_height, new_width = height // 2, width // 2
+
+    # # Resize the image
+    # frame = cv2.resize(frame, (new_width, new_height))
+
     startTime = time.perf_counter()
-    results = model.predict(frame, conf=0.5)
+    results = model.predict(frame, conf=CONFIDENCE)
     result = results[0]
     detections = make_detections(result.boxes)
     tracks = object_tracker.update_tracks(detections, frame=frame)
     # print(result.boxes)
 
-    for track in tracks:
+    for index, track in enumerate(tracks):
         if not track.is_confirmed():
             continue
         trackId = track.track_id
         ltrb = track.to_ltrb()
         bbox = ltrb
-        # print(track)
+        # print(
+        #     f"{index} and {track} and {track.is_confirmed()}")
         cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(
             bbox[2]), int(bbox[3])), (0, 0, 255), 2)
         cv2.putText(frame, f"{str(trackId)}", (int(bbox[0]), int(
